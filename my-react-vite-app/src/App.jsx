d2a89bc47a69d0b99c177a6515d1f1a44cb13791
hieuFPTAptech <hieuhiphop797@gmail.com>
2025-09-21 08:16:16 +0700

    lawyer list

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -3,8 +3,8 @@ import HomeTemplate from "./template/homeTemplate/homeTemplate";
 import Home from "./page/Home";
 import Login from "./page/Login";
 import Register from "./page/register";
-
-
+import LawyerList from "./page/LawyerList";
+import PopUp from "./page/PopUp";
 
 export default function App() {
   return (
@@ -13,8 +13,9 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
+        <Route path="/lawyerList" element={<LawyerList />} />
       </Route>
+      <Route path="/popup" element={<PopUp />} />
     </Routes>
   );
 }
---
+++ my-react-vite-app/src/page/LawyerList.jsx
@@ -0,0 +1,259 @@
+import React from "react";
+import "../../public/assets/style/LawyerList.css";
+import "../../public/assets/style/LawyerListCarousel.css";
+
+export default function LawyerList() {
+  return (
+    <>
+      <section className="lawyerList">
+        <section className="blog-card">
+          <div className="container">
+            <div className="card-wrap d-flex justify-content-between flex-wrap">
+              <div className="blog-card-items w-100">
+                <a href="">
+                  <div className="card">
+                    <img
+                      src="./assets/img/lawyer-1.jpg"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="date">
+                      <span className="text-light fw-bold fs-5">
+                        26 Jan, 2020
+                      </span>
+                    </div>
+
+                    <div className="card-body mx-2">
+                      <label className="text-color1 bg-color1 fw-bold my-3">
+                        Business
+                      </label>
+                      <h5 className="card-title fw-bold">
+                        Corporate, Business & Digital Agencye
+                      </h5>
+                      <p className="card-text py-3">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a
+                        href="#"
+                        className="text-color1 fs-6 fw-bold pt-2 my-2 d-inline-block"
+                      >
+                        Know more
+                        <i className="fa fa-arrow-right text-color1 fw-bold"></i>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
+
+              <div className="blog-card-items w-100">
+                <a href="">
+                  <div className="card">
+                    <img
+                      src="./assets/img/lawyer-1.jpg"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="date">
+                      <span className="text-light fw-bold fs-5">
+                        26 Jan, 2020
+                      </span>
+                    </div>
+
+                    <div className="card-body mx-2">
+                      <label className="text-color1 bg-color1 fw-bold my-3">
+                        Business
+                      </label>
+                      <h5 className="card-title fw-bold">
+                        Corporate, Business & Digital Agencye
+                      </h5>
+                      <p className="card-text py-3">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a
+                        href="#"
+                        className="text-color1 fs-6 fw-bold pt-2 my-2 d-inline-block"
+                      >
+                        Know more
+                        <i className="fa fa-arrow-right text-color1 fw-bold"></i>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="blog-card-items w-100">
+                <a href="">
+                  <div className="card">
+                    <img
+                      src="./assets/img/lawyer-2.jpg"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="date">
+                      <span className="text-light fw-bold fs-5">
+                        26 Jan, 2020
+                      </span>
+                    </div>
+
+                    <div className="card-body mx-2">
+                      <label className="text-color1 bg-color1 fw-bold my-3">
+                        Business
+                      </label>
+                      <h5 className="card-title fw-bold">
+                        Corporate, Business & Digital Agencye
+                      </h5>
+                      <p className="card-text py-3">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a
+                        href="#"
+                        className="text-color1 fs-6 fw-bold pt-2 my-2 d-inline-block"
+                      >
+                        Know more
+                        <i className="fa fa-arrow-right text-color1 fw-bold"></i>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="blog-card-items w-100">
+                <a href="">
+                  <div className="card">
+                    <img
+                      src="./assets/img/lawyer-3.jpg"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="date">
+                      <span className="text-light fw-bold fs-5">26 Jan, 2020</span>
+                    </div>
+
+                    <div className="card-body mx-2">
+                      <label className="text-color1 bg-color1 fw-bold my-3">
+                        Business
+                      </label>
+                      <h5 className="card-title fw-bold">
+                        Corporate, Business & Digital Agencye
+                      </h5>
+                      <p className="card-text py-3">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a
+                        href="#"
+                        className="text-color1 fs-6 fw-bold pt-2 my-2 d-inline-block"
+                      >
+                        Know more
+                        <i className="fa fa-arrow-right text-color1 fw-bold"></i>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="blog-card-items w-100">
+                <a href="">
+                  <div className="card">
+                    <img
+                      src="./assets/img/lawyer-1.jpg"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="date">
+                      <span className="text-light fw-bold fs-5">26 Jan, 2020</span>
+                    </div>
+
+                    <div className="card-body mx-2">
+                      <label className="text-color1 bg-color1 fw-bold my-3">
+                        Business
+                      </label>
+                      <h5 className="card-title fw-bold">
+                        Corporate, Business & Digital Agencye
+                      </h5>
+                      <p className="card-text py-3">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a
+                        href="#"
+                        className="text-color1 fs-6 fw-bold pt-2 my-2 d-inline-block"
+                      >
+                        Know more
+                        <i className="fa fa-arrow-right text-color1 fw-bold"></i>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <div className="blog-card-items w-100">
+                <a href="">
+                  <div className="card">
+                    <img
+                      src="./assets/img/lawyer-2.jpg"
+                      className="card-img-top "
+                      alt="..."
+                    />
+                    <div className="date">
+                      <span className="text-light fw-bold fs-5">26 Jan, 2020</span>
+                    </div>
+
+                    <div className="card-body mx-2">
+                      <label className="text-color1 bg-color1 fw-bold my-3">
+                        Business
+                      </label>
+                      <h5 className="card-title fw-bold">
+                        Corporate, Business & Digital Agencye
+                      </h5>
+                      <p className="card-text py-3">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                      <a
+                        href="#"
+                        class="text-color1 fs-6 fw-bold pt-2 my-2 d-inline-block"
+                      >
+                        Know more
+                        <i class="fa fa-arrow-right text-color1 fw-bold"></i>
+                      </a>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+
+            <div class="pagination-wrap mt-5">
+              <ul class="pagination justify-content-center">
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    <i class="fa fa-angle-double-left"></i>
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link active" href="#">
+                    1
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    2
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    3
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    <i class="fa fa-angle-double-right"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+}
