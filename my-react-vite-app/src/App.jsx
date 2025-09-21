af07c867ff83a2495102c8ad55299a55a3999d66
hieuFPTAptech <hieuhiphop797@gmail.com>
2025-09-21 08:23:27 +0700

    lawyer list

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -1,10 +1,9 @@
 import { Routes, Route, Outlet } from "react-router-dom";
-import HomeTemplate from "./template/homeTemplate/homeTemplate";
+import HomeTemplate from "./template/homeTemplate/HomeTemplate";
 import Home from "./page/Home";
 import Login from "./page/Login";
 import Register from "./page/register";
 import LawyerList from "./page/LawyerList";
-import PopUp from "./page/PopUp";
 
 export default function App() {
   return (
@@ -15,7 +14,6 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/lawyerList" element={<LawyerList />} />
       </Route>
-      <Route path="/popup" element={<PopUp />} />
     </Routes>
   );
 }
