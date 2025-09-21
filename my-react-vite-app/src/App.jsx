ab1eddc154840a6f06cbe3084e13c69c5f09f075
hieuFPTAptech <hieuhiphop797@gmail.com>
2025-09-21 08:24:40 +0700

    list

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import HomeTemplate from "./template/homeTemplate/HomeTemplate";
 import Home from "./page/Home";
 import Login from "./page/Login";
-import Register from "./page/register";
+import Register from "./page/Register";
 import LawyerList from "./page/LawyerList";
 
 export default function App() {
